2bec29c69817e6a14a87feb49a38483c5ddac3b4
MrGreenNV <averkievnv@gmail.com>
2023-08-19 18:51:28 +0300

    update CustomEmailValidation.java, CustomEmail.java

--- src/main/java/ru/averkiev/socialmediaapi/validations/CustomEmail.java
+++ src/main/java/ru/averkiev/socialmediaapi/validations/CustomEmail.java
@@ -1,28 +1,36 @@
 package ru.averkiev.socialmediaapi.validations;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
 /**
+ * Аннотация для валидации электронной почты.
  * @author mrGreenNV
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = CustomEmailValidation.class)
-@ApiModel(description = "Аннотация для валидации электронной почты")
 public @interface CustomEmail {
 
-    @ApiModelProperty(notes = "Определяет сообщение, которое будет отображаться при нарушении валидации")
+    /**
+     * Определяет сообщение, которое будет отображаться при нарушении валидации.
+     * @return сообщение.
+     */
     String message() default "Email должен быть валидным";
 
-    @ApiModelProperty(notes = "Определяет группы ограничений, которым будет принадлежать аннотация")
+    /**
+     * Определяет группы ограничений, которым будет принадлежать аннотация.
+     * @return группы ограничений.
+     */
     Class<?>[] groups() default {};
 
-    @ApiModelProperty(notes = "Определяет нагрузку (payload) для аннотации, которая может быть использована для передачи дополнительной информации в процессе валидации")
+    /**
+     * Определяет нагрузку (payload) для аннотации, которая может быть использована для передачи дополнительной
+     * информации в процессе валидации.
+     * @return нагрузка для аннотации.
+     */
     Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/ru/averkiev/socialmediaapi/validations/CustomEmailValidation.java
+++ src/main/java/ru/averkiev/socialmediaapi/validations/CustomEmailValidation.java
@@ -1,30 +1,38 @@
 package ru.averkiev.socialmediaapi.validations;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.regex.Pattern;
 
 /**
+ * Класс представляет собой валидатор для проверки электронной почты.
  * @author mrGreenNV
  */
-@ApiModel(description = "Валидация электронной почты")
 public class CustomEmailValidation implements ConstraintValidator<CustomEmail, String> {
 
-    @ApiModelProperty(notes = "Регулярное выражение для проверки электронной почты")
+    /**
+     * Регулярное выражение для проверки электронной почты.
+     */
     private final static Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+.+.[A-Za-z]{2,4}$");
 
+    /**
+     * Инициализирует валидацию, устанавливая граничные значения.
+     * @param constraintAnnotation аннотация
+     */
     @Override
-    @ApiModelProperty(notes = "Инициализирует валидацию с установкой граничных значений")
     public void initialize(CustomEmail constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
+    /**
+     * Проверяет валидность электронной почты.
+     * @param value проверяемая электронная почта.
+     * @param context контекст.
+     * @return true если электронная почта валидна, иначе false
+     */
     @Override
-    @ApiModelProperty(notes = "Проверяет валидность электронной почты")
-    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(String value, ConstraintValidatorContext context) {
 
         if (value == null) {
             return true;
