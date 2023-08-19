420c8c047f8525bb4a249f1b9b4ec13775074115
MrGreenNV <averkievnv@gmail.com>
2023-08-19 18:46:57 +0300

    add BaseEntity.java

---
+++ src/main/java/ru/averkiev/socialmediaapi/models/BaseEntity.java
@@ -0,0 +1,46 @@
+package ru.averkiev.socialmediaapi.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+/**
+ * Базовый класс сущностей.
+ * @author mrGreenNV
+ */
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+
+    /**
+     * Идентификатор сущности.
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Время создания сущности.
+     */
+    @Column(name = "create_at")
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    /**
+     * Время обновления сущности.
+     */
+    @Column(name = "update_at")
+    @UpdateTimestamp
+    private LocalDate updateAt;
+
+    /**
+     * Статус сущности в системе.
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+}
