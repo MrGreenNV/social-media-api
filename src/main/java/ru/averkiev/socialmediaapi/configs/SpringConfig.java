d416fd5890ec4b389676a6788a8327fce4691997
MrGreenNV <averkievnv@gmail.com>
2023-08-19 21:00:40 +0300

    add SpringConfig.java

---
+++ src/main/java/ru/averkiev/socialmediaapi/configs/SpringConfig.java
@@ -0,0 +1,28 @@
+package ru.averkiev.socialmediaapi.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Настройка конфигурации Spring.
+ * @author mrGreenNV
+ */
+@Configuration
+@ComponentScan
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class SpringConfig {
+
+    /**
+     * Создание Bean для преобразования DTO к модели и наоборот.
+     * @return объект ModelMapper.
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
