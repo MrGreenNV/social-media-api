a5402ad30acd0a391c482c989d3a447eb369b5d5
MrGreenNV <averkievnv@gmail.com>
2023-08-19 20:17:06 +0300

    add UserService.java

---
+++ src/main/java/ru/averkiev/socialmediaapi/services/UserService.java
@@ -0,0 +1,52 @@
+package ru.averkiev.socialmediaapi.services;
+
+import ru.averkiev.socialmediaapi.exceptions.RegistrationException;
+import ru.averkiev.socialmediaapi.exceptions.UserNotFoundException;
+import ru.averkiev.socialmediaapi.models.User;
+import ru.averkiev.socialmediaapi.models.UserCreateDTO;
+
+import java.util.List;
+
+/**
+ * Интерфейс определяет функциональность для управления пользователями.
+ * @author mrGreenNV
+ */
+public interface UserService {
+
+    /**
+     * Регистрирует нового пользователя в системе.
+     * @param userCreateDTO DTO данные нового пользователя.
+     * @return данные зарегистрированного пользователя.
+     * @throws RegistrationException выбрасывает если регистрация пользователя не удалась по каким-либо причинам.
+     */
+    UserCreateDTO register(UserCreateDTO userCreateDTO) throws RegistrationException;
+
+    /**
+     * Сохраняет нового пользователя в системе.
+     * @param user новый пользователь.
+     * @return созданный пользователь.
+     */
+    User saveUser(User user);
+
+    /**
+     * Возвращает пользователя по его идентификатору.
+     * @param userId идентификатор искомого пользователя.
+     * @return Optional, содержащий найденного пользователя, или пустой Optional, если пользователь не найден.
+     * @throws UserNotFoundException выбрасывает, если пользователь с заданным идентификатором не найден.
+     */
+    User getUserById(Long userId) throws UserNotFoundException;
+
+    /**
+     * Возвращает пользователя по его логину.
+     * @param username имя пользователя в системе.
+     * @return Optional, содержащий найденного пользователя, или пустой Optional, если пользователь не найден.
+     * @throws UserNotFoundException выбрасывает, если пользователь с заданным именем не найден.
+     */
+    User getUserByLogin(String username) throws UserNotFoundException;
+
+    /**
+     * Возвращает список всех пользователей.
+     * @return список пользователей.
+     */
+    List<User> getAllUsers();
+}
