4a3e45501a4d7cbe83663f73a106279b8d635c27
MrGreenNV <averkievnv@gmail.com>
2023-08-19 18:47:49 +0300

    add Status.java

---
+++ src/main/java/ru/averkiev/socialmediaapi/models/Status.java
@@ -0,0 +1,23 @@
+package ru.averkiev.socialmediaapi.models;
+
+/**
+ * Статус сущностей в системе.
+ * @author mrGreenNV
+ */
+public enum Status {
+
+    /**
+     * Сущность активна.
+     */
+    ACTIVE,
+
+    /**
+     * Сущность неактивна.
+     */
+    NOT_ACTIVE,
+
+    /**
+     * Сущность помечена на удаление.
+     */
+    DELETED
+}
