d5a750e3668a4d25aebfebc538ded5ecd5b77a95
MrGreenNV <averkievnv@gmail.com>
2023-08-20 22:02:20 +0300

    update UserFriendDTO.java

--- src/main/java/ru/averkiev/socialmediaapi/models/UserFriendDTO.java
+++ src/main/java/ru/averkiev/socialmediaapi/models/UserFriendDTO.java
@@ -1,5 +1,6 @@
 package ru.averkiev.socialmediaapi.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,14 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "Сущность пользователя с данными о пользователе")
 public class UserFriendDTO {
 
     /** Имя пользователя в системе. */
+    @Schema(description = "Имя пользователя в системе", example = "new_user2023")
     private String username;
 
     /** Электронная почта пользователя. */
+    @Schema(description = "Электронная почта пользователя", example = "new_user2023@gamil.com")
     private String email;
 }
