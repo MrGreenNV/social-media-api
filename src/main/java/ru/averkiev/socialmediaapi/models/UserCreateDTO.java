95b9b6d3f32108810fdaab7d1557e28311e94c2b
MrGreenNV <averkievnv@gmail.com>
2023-08-20 21:29:40 +0300

    update OpenApiConfig.java, UserCreateDTO.java

--- src/main/java/ru/averkiev/socialmediaapi/models/UserCreateDTO.java
+++ src/main/java/ru/averkiev/socialmediaapi/models/UserCreateDTO.java
@@ -1,5 +1,6 @@
 package ru.averkiev.socialmediaapi.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import ru.averkiev.socialmediaapi.validations.CustomEmail;
@@ -11,16 +12,20 @@ import ru.averkiev.socialmediaapi.validations.CustomUsername;
  */
 @Getter
 @Setter
+@Schema(description = "Сущность пользователя с данными для регистрации в системе")
 public class UserCreateDTO {
 
     /** Имя пользователя в системе. */
+    @Schema(description = "Имя пользователя в системе", example = "new_user2023")
     @CustomUsername
     private String username;
 
     /** Хэшированный пароль пользователя. */
+    @Schema(description = "Пароль пользователя")
     private String password;
 
     /** Электронная почта пользователя. */
+    @Schema(description = "Электронная почта пользователя", example = "new_user2023@gamil.com")
     @CustomEmail
     private String email;
 
--- src/main/java/ru/averkiev/socialmediaapi/utils/OpenApiConfig.java
+++ src/main/java/ru/averkiev/socialmediaapi/utils/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package ru.averkiev.socialmediaapi.utils;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 /**
  * Класс для настройки конфигурации Swagger.
@@ -18,6 +20,12 @@ import io.swagger.v3.oas.annotations.info.Info;
                         url = "https://github.com/MrGreenNV"
                 )
         )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class OpenApiConfig {
 }
