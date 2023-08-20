dde122c55347f0944632e0136e3c46223301c6e1
MrGreenNV <averkievnv@gmail.com>
2023-08-20 12:45:17 +0300

    update RefreshTokenRepository.java, AccessTokenRepository.java

--- src/main/java/ru/averkiev/socialmediaapi/repositories/AccessTokenRepository.java
+++ src/main/java/ru/averkiev/socialmediaapi/repositories/AccessTokenRepository.java
@@ -1,6 +1,7 @@
 package ru.averkiev.socialmediaapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.averkiev.socialmediaapi.security.AccessToken;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
  * Интерфейс представляет собой репозиторий access токенов.
  * @author mrGreenNV
  */
+@Repository
 public interface AccessTokenRepository extends JpaRepository<AccessToken, Long> {
     Optional<AccessToken> findByUserId(long userId);
 
--- src/main/java/ru/averkiev/socialmediaapi/repositories/RefreshTokenRepository.java
+++ src/main/java/ru/averkiev/socialmediaapi/repositories/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package ru.averkiev.socialmediaapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.averkiev.socialmediaapi.security.RefreshToken;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
  * Интерфейс представляет собой репозиторий refresh токенов.
  * @author mrGreenNV
  */
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(long userId);
 }
