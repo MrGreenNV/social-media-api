1c1cac5a79e25b7a947f5532d32cbd0061fe3ac3
MrGreenNV <averkievnv@gmail.com>
2023-08-23 09:39:14 +0300

    update UserFriendRepository.java add UserFriendServiceImpl.java

--- src/main/java/ru/averkiev/socialmediaapi/repositories/UserFriendRepository.java
+++ src/main/java/ru/averkiev/socialmediaapi/repositories/UserFriendRepository.java
@@ -2,7 +2,10 @@ package ru.averkiev.socialmediaapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.averkiev.socialmediaapi.models.User;
 import ru.averkiev.socialmediaapi.models.UserFriend;
+
+import java.util.Optional;
 
 /**
  * Интерфейс представляет собой функциональность взаимодействия объекта UserFriend с базой данных.
@@ -10,4 +13,19 @@ import ru.averkiev.socialmediaapi.models.UserFriend;
  */
 @Repository
 public interface UserFriendRepository extends JpaRepository<UserFriend, Long> {
+
+    /**
+     * Выполняет поиск дружеской связи между пользователями в базе данных.
+     * @param user пользователь.
+     * @param friend друг пользователя.
+     * @return Optional содержащий дружескую связь, если она была найдена, иначе - пустой.
+     */
+    Optional<UserFriend> findUserFriendByUserAndFriend(User user, User friend);
+
+    /**
+     * Выполняет проверку существования записи о дружеской связи между пользователями в базе данных.
+     * @param userFriend информация о дружеской связи.
+     * @return true, если удалось найти запись, иначе - false.
+     */
+    boolean exists(UserFriend userFriend);
 }
